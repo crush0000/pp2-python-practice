12ec5a83950989bd116ef0b033d0a9784b0e2cd3
Aldiyar <aldiyar20050507@>
2024-01-18 19:27:58 +0600

    variables

---
+++ lab 1/variables.py
@@ -0,0 +1,28 @@
+#ex 1
+carname = "Volvo"
+
+#ex 2
+x = 50
+
+#ex 3
+x = 5
+y = 10
+print(x + y)
+
+#ex4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#ex5
+x,y,z = "Orange","Banana","Cherry"
+
+#ex6
+x=y=z = "Orange"
+
+#ex7
+def myfunc():
+    global x
+    x = "fantastic"
+
